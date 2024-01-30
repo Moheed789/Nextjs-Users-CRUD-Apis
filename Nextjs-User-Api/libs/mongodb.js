55295e0f776262402266eec7f1de20d895dc9a6a
Moheed789 <moheedsiddiqui74@gmail.com>
2024-01-30 15:46:18 -0800

    Nextjs User Api

--- Nextjs-User-Api/libs/mongodb.js
+++ Nextjs-User-Api/libs/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectMongoDB = () => {
     try {
         mongoose.connect(process.env.MONGODB_URI);
-        console.log("Connected to MongoDB.")
+        console.log("Connected to Database.")
     } catch (error) {
         console.log("Database Error", error)
     }
